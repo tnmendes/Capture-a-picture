eb98772dcb26e46d4d4511b6360e2d8815485052
Tiago Mendes <>
2018-10-14 23:35:06 +0100

    Empty Project without storyboard

---
+++ Capture-a-picture/Capture-a-picture/Class/Controllers/WellcomeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  WellcomeViewController.swift
+//  Capture-a-picture
+//
+//  Created by Tiago Mendes on 14/10/2018.
+//  Copyright © 2018 Tiago Mendes. All rights reserved.
+//
+
+import UIKit
+
+class WellcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.blue
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
